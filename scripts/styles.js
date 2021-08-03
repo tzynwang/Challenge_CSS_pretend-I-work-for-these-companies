14a75b1a307c58d9a0b7444a1ad39eea2a374a58
tzyn (windows) <tzyn.wang@gmail.com>
2021-08-03 17:34:54 +0800

    feat: nav bar desktop layout style

--- scripts/styles.js
+++ scripts/styles.js
@@ -2,8 +2,8 @@ const headerCheckbox = document.querySelector('.header__nav-icon--checkbox')
 
 headerCheckbox.addEventListener('change', event => {
   if (event.target.checked) {
-    document.querySelector('.nav').style.top = '60px'
+    document.querySelector('.nav--mobile').style.top = '60px'
   } else {
-    document.querySelector('.nav').style.top = 'calc(-100vh - 60px)'
+    document.querySelector('.nav--mobile').style.top = 'calc(-100vh - 60px)'
   }
 })
