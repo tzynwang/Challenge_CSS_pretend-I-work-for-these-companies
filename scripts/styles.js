4b9281cf94cbbef0a94eaa1bfb6fef2418b2506b
tzyn (windows) <tzyn.wang@gmail.com>
2021-08-03 19:30:35 +0800

    feat: introduction section style

--- scripts/styles.js
+++ scripts/styles.js
@@ -1,9 +1,16 @@
 const headerCheckbox = document.querySelector('.header__nav-icon--checkbox')
+const modeSwitch = document.querySelector('.service-call')
 
+// mobile nav-bar open/close
 headerCheckbox.addEventListener('change', event => {
   if (event.target.checked) {
-    document.querySelector('.nav--mobile').style.top = '60px'
+    document.querySelector('.nav--mobile').style.top = '80px'
   } else {
-    document.querySelector('.nav--mobile').style.top = 'calc(-100vh - 60px)'
+    document.querySelector('.nav--mobile').style.top = 'calc(-100vh - 80px)'
   }
+})
+
+// dark mode
+modeSwitch.addEventListener('click', () => {
+  document.documentElement.setAttribute('data-theme', 'dark')
 })
