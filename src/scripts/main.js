00648062f9455526ba81c6a6832605eea366f70b
tzyn (windows) <tzyn.wang@gmail.com>
2021-08-04 16:35:41 +0800

    feat: initial setting

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+import '../styles/main.scss'
+import Vue from 'vue'
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'hello world'
+  }
+})
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/scripts/main.js',
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm.js' // development build with compiler
+      // vue: 'vue/dist/vue.runtime.esm.js' // product runtime without compiler
+    }
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'production'),
+    clean: true
+  }
+}
